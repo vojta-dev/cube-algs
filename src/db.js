470dd0f3412bdc67f6a0b067bf00fdcf3e5c6650
Vojta <65811166+vojta-dev@users.noreply.github.com>
2022-03-05 20:15:59 +0100

    Add oll links and add some pll algs

--- src/db.js
+++ src/db.js
@@ -5,6 +5,20 @@ const data = [
   {
     name: 'OLL',
     cube: { main: '-YY YYY -Y-', sides: '... ..Y ..Y ..Y' },
+    links: [
+      {
+        name: 'CubeSkills (pdf)',
+        link: 'https://www.cubeskills.com/uploads/pdf/tutorials/oll-algorithms.pdf',
+      },
+      {
+        name: 'AlgDb (website)',
+        link: 'http://algdb.net/puzzle/333/oll',
+      },
+      {
+        name: 'J Perm (video)',
+        link: 'https://www.youtube.com/watch?v=vU6HsK3hvQs',
+      },
+    ],
     algs: [],
   },
   {
@@ -25,8 +39,27 @@ const data = [
       },
     ],
     algs: [
-      { name: 'Aa', cube: { main: 'Y', sides: 'RGR BRO GBB OOG' }, algs: ["x R' U R' D2 R U' R' D2 R2", "l' U R' D2 R U' R' D2 R2	"] },
-      { name: 'Ab', cube: { main: 'Y', sides: 'RGR BRO GBB OOG' } },
+      { name: 'Aa', cube: { main: 'Y', sides: 'RGR BRO GBB OOG' }, algs: ["x R' U R' D2 R U' R' D2 R2"] },
+      { name: 'Ab', cube: { main: 'Y', sides: 'RRO GBR BOB OGG' }, algs: ["x' R U' R D2 R' U R D2 R2"] },
+      { name: 'E', cube: { main: 'Y', sides: 'BOG RGO GRB OBR' }, algs: ["x' R U' R' D R U R' D' R U R' D R U' R' D'"] },
+      { name: 'F', cube: { main: 'Y', sides: 'GBR BRG RGB OOO' }, algs: ["R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R"] },
+      { name: 'Ga', cube: { main: 'Y', sides: 'GBR BOG RRB OGO' }, algs: [] },
+      { name: 'Gb', cube: { main: 'Y', sides: 'GOR BBG RGB ORO' }, algs: [] },
+      { name: 'Gc', cube: { main: 'Y', sides: 'GRR BOG RGB OBO' }, algs: [] },
+      { name: 'Gd', cube: { main: 'Y', sides: 'GBR BGG ROB ORO' }, algs: [] },
+      { name: 'H', cube: { main: 'Y', sides: 'GBG ROR BGB ORO' }, algs: [] },
+      { name: 'Ja', cube: { main: 'Y', sides: 'OGG RRR BOO GBB' }, algs: [] },
+      { name: 'Jb', cube: { main: 'Y', sides: 'GGR BBG RRB OOO' }, algs: [] },
+      { name: 'Na', cube: { main: 'Y', sides: 'RRO GGB OOR BBG' }, algs: [] },
+      { name: 'Nb', cube: { main: 'Y', sides: 'ROO GBB ORR BGG' }, algs: [] },
+      { name: 'Ra', cube: { main: 'Y', sides: 'GOB OGG RBR BRO' }, algs: [] },
+      { name: 'Rb', cube: { main: 'Y', sides: 'BRG ROB OBO GGR' }, algs: [] },
+      { name: 'T', cube: { main: 'Y', sides: 'GGR BOG RBB ORO' }, algs: [] },
+      { name: 'Ua', cube: { main: 'Y', sides: 'GOG RGR BBB ORO' }, algs: [] },
+      { name: 'Ub', cube: { main: 'Y', sides: 'GRG ROR BBB OGO' }, algs: [] },
+      { name: 'V', cube: { main: 'Y', sides: 'OBR BRG ROO GGB' }, algs: [] },
+      { name: 'Y', cube: { main: 'Y', sides: 'BOG RRO GBB OGR' }, algs: [] },
+      { name: 'Z', cube: { main: 'Y', sides: 'BRB OGO GOG RBR' }, algs: [] },
     ],
   },
 ];
